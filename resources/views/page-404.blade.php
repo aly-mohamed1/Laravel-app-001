54570d0a331ee52453013c1dd8330056bbfcb168
Aly Mohamed <aly.mohamed2707@gmail.com>
2026-04-14 09:51:39 +0200

    Redirect to welcome page

--- resources/views/page-404.blade.php
+++ resources/views/page-404.blade.php
@@ -6,5 +6,6 @@
     <body style="text-align: center; margin: top 50px;">
         <h1>404 Not Found</h1>
         <p>The requested page could not be found.</p>
+        <a href="/">Go back to Home</a>
     </body>
 </html>
