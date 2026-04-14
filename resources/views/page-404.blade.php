39a27d3a0d276d0c9adb3a7183cd8c7327db02a9
Aly Mohamed <aly.mohamed2707@gmail.com>
2026-04-14 09:47:12 +0200

    Creating a customized 404 page

---
+++ resources/views/page-404.blade.php
@@ -0,0 +1,10 @@
+<html>
+
+    <head>
+        <title>404 Not Found</title>
+    </head>
+    <body style="text-align: center; margin: top 50px;">
+        <h1>404 Not Found</h1>
+        <p>The requested page could not be found.</p>
+    </body>
+</html>
