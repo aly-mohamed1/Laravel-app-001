2dcdd667fcbb822030995558d6b777564ab1e001
Aly Mohamed <aly.mohamed2707@gmail.com>
2026-04-16 16:07:32 +0200

    Creating static functions

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -6,5 +6,24 @@ use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
-    //
+    public static function index(){
+        return "All tasks";
+    }
+
+    public static function create(){
+        echo "<input />";
+    }
+
+    public static function edit($tasks){
+        echo "<input value= '$tasks' />";
+    }
+
+    public static function show($tasks){
+        return "Task $tasks page";
+    }
+
+    public static function store(Request $request){
+        return $request->all();
+    }
+
 }
