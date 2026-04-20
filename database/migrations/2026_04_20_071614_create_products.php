89f78c19433ed74b44e464eba8dbadb103ec9d90
Aly Mohamed <aly.mohamed2707@gmail.com>
2026-04-20 10:05:29 +0200

    Adding extra columns

--- database/migrations/2026_04_20_071614_create_products.php
+++ database/migrations/2026_04_20_071614_create_products.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('title', 50); //Aadding a column char(50) this is the limit number of characters
+            $table->text('description'); // Adding a coumn its type is text
+            $table->float('price'); // Adding a coumn its type is numeric
             $table->timestamps();
         });
     }
