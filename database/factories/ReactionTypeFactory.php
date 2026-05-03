b4ad929dd2380061932c47f9d0ac969515ebde40
Aly Mohamed <aly.mohamed2707@gmail.com>
2026-05-03 14:35:49 +0300

    prepare all routes

---
+++ database/factories/ReactionTypeFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\ReactionType;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<ReactionType>
+ */
+class ReactionTypeFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
