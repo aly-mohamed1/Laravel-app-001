a666ff68dca15881480709c5f88de378db9ae7e3
Aly Mohamed <aly.mohamed2707@gmail.com>
2026-05-13 09:07:16 +0300

    validation

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,55 +4,62 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\LoginRequest;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
     public function login(LoginRequest $request) {
-        $data = $request->validated();
+        $credentials = $request->validated();
 
-        return $data;
+        $auth = Auth::attempt($credentials);
+
+        if($auth) {
+            $user = auth()->user();
+            $abilities = [$user->roles];
+            $token = $user->createToken('login', $abilities)->plainTextToken;
+
+            // Method1: Generate array of user and token
+            // return [
+            //     'token' => $token,
+            //     'user' => $user,
+            // ];
+
+        // Method2: Generate array of user and token
+        $user->token = $token;
+        return $user;
+
+        } else {
+            return response("User doesn't exist");
+        }
+        //return $auth;
+        //$x = auth()
+        //return $data;
     }
 
     public function register() {}
 
+
     public function logout() {}
-    /**
-     *
-     */
+
 
     public function forget_password() {}
-    /**
-     *
-     */
+
 
     public function reset_password() {}
-    /**
-     *
-     */
+
 
     public function change_password() {}
-    /**
-     *
-     */
+
 
     public function active_sessions() {}
-    /**
-     *
-     */
 
     public function logout_all() {}
-    /**
-     *
-     */
+
 
     public function logout_current() {}
-    /**
-     *
-     */
+
 
     public function logout_others() {}
-    /**
-     *
-     */
+
 
 }
